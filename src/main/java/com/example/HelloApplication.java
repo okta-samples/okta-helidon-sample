84f997a331925708eb71de62db701f6086e00981
Matt Raible <matt.raible@okta.com>
2022-05-19 12:09:39 +0000

    Add security.providers.1.oidc.oidc-metadata-well-known=false

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import com.example.controller.HelloResource;
+import com.example.controller.HomeResource;
 import org.eclipse.microprofile.auth.LoginConfig;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.Application;
+
 import java.util.Set;
 
 @LoginConfig(authMethod = "MP-JWT")
@@ -13,6 +15,6 @@ public class HelloApplication extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        return Set.of(HelloResource.class);
+        return Set.of(HelloResource.class, HomeResource.class);
     }
 }
